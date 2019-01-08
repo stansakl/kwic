f1bb370be95b9a552c63013c6485cc5a6beea61e
stansakl <stansakl@users.noreply.github.com>
2019-01-07 19:51:03 -0600

    Update kwic.py
    Open and read file

--- kwic_python/kwic.py
+++ kwic_python/kwic.py
@@ -23,3 +23,15 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+if __name__ == '__main__':
+    print("Running kwic.py")
+
+    file = open("input.txt", "r", 1)
+    lines = file.readlines()
+    count = 0
+    for line in lines:
+        count = count + 1
+        print("Found line %d: %s" % (count, line))
+        
+    file.close()
