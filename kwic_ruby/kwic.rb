8d8ac5099425cb33839bd1ce4f0ef7b05f41dbf1
stansakl <stansakl@users.noreply.github.com>
2019-01-08 19:56:17 -0600

    Open file and print lines to stdout

--- kwic_ruby/kwic.rb
+++ kwic_ruby/kwic.rb
@@ -23,3 +23,12 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+puts("Running kwic in Ruby")
+
+
+inputFile = File.open('input.txt').read
+
+inputFile.each_line do |line|
+    print "#{line}"
+  end
